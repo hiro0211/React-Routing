e5d9a7cf982c782ce73766caa666736f9cec0424
hiro0211 <143386971+hiro0211@users.noreply.github.com>
2024-11-28 22:48:17 +0900

    Initial commit

---
+++ src/Page1.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <h1>1ページ</h1>
+    </div>
+  );
+};
---
+++ src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>2ページ</h1>
+    </div>
+  );
+};
